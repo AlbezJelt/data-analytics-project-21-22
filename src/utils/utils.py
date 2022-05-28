eb1cb4c2442775c3d3240288e7243dbfa7fd16c1
Federico Alberici <fed.alberici@gmail.com>
2022-05-28 15:43:18 +0000

    First basic analytics;

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,5 +1,13 @@
 import igraph as ig
 import itertools as it
+import numpy as np
 
 def layout_geo(g: ig.Graph) -> ig.Layout:
     return ig.Layout(it.zip_longest(g.vs['lat'], g.vs['lon']))
+
+def top_n_indices(arr: np.ndarray, n: int, sort: bool = True) -> np.ndarray:
+    top_n = np.argpartition(arr, -n)[-n:]
+    if sort:
+        return top_n[np.argsort(arr[top_n])[::-1][:n]]
+    else:
+        return top_n
